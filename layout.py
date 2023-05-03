344fb5cfe9a8bbddf69adb7dca58053cdc61b68e
shadow-smoke <tylerjenkins721@gmail.com>
2023-05-03 16:20:02 -0400

    Map stuff

--- layout.py
+++ layout.py
@@ -7,7 +7,10 @@ def create_layout(app: Dash, data) -> html.Div:
         className="app-div",
         children=[
             html.H1(app.title),
-            dcc.Graph(figure=px.scatter(
-                data, x='indicator', y='human development index (2021)'))
+            # dcc.Graph(figure=px.scatter(
+            #     data, x='indicator', y='human development index (2021)')),
+            dcc.Graph(figure=px.choropleth(data, locations="indicator", locationmode="country names",
+                                           color="population (2018)", hover_name="indicator",
+                                           range_color=[0, 1000], color_continuous_scale="Blues"))
         ]
     )
