f44a201d2f15b6a0f8d5c6c4ef9db1ba096a6721
Omeed Zarrabian <60447674+OmeedZ@users.noreply.github.com>
2023-05-03 17:53:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,24 +10,36 @@ def load_data(path: str) -> pd.DataFrame:
     )
     return data
 
-def create_layout(app: Dash, data) -> html.Div:
+def create_map(app: Dash, data) -> html.Div:
     return html.Div(
         className="app-div",
         children=[
             html.H1(app.title),
-            # dcc.Graph(figure=px.scatter(
-            #     data, x='indicator', y='human development index (2021)')),
             dcc.Graph(figure=px.choropleth(data, locations="indicator", locationmode="country names",
                                            color="population (2018)", hover_name="indicator",
                                            range_color=[0, 1000], color_continuous_scale="Blues"))
         ]
     )
 
+def create_chart(app: Dash, data) -> html.Div:
+    df = px.data.iris()
+    fig = px.scatter(df, x="sepal_width", y="sepal_length")
+    return html.Div(
+        className="app-div1",
+        children=[
+            html.H1(app.title),
+            dcc.Graph(figure=fig)
+        ]
+    )
+
+
+
 def main() -> None:
     data = load_data("data.csv")
     app = Dash(external_stylesheets=[BOOTSTRAP])
     app.title = "471 Final Project"
-    app.layout = create_layout(app, data=data)
+    app.layout = create_map(app, data=data)
+    app.layout = create_chart(app, data=data)
     app.run()
 
 
