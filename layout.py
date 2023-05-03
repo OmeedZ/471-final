142838ca381cfae48324222859dee06187653529
shadow-smoke <tylerjenkins721@gmail.com>
2023-05-03 15:55:44 -0400

    Added basic visualization

--- layout.py
+++ layout.py
@@ -7,7 +7,6 @@ def create_layout(app: Dash, data) -> html.Div:
         className="app-div",
         children=[
             html.H1(app.title),
-            dash_table.DataTable(data=data.to_dict('records'), page_size=10),
             dcc.Graph(figure=px.scatter(
                 data, x='indicator', y='human development index (2021)'))
         ]
