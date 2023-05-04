7ead553212a531baab7213cbe40adbb541677af6
Omeed Zarrabian <60447674+OmeedZ@users.noreply.github.com>
2023-05-04 01:39:26 -0400

    chloropleth map working

--- main.py
+++ main.py
@@ -1,50 +1,59 @@
-import pandas as pd
-from dash import Dash, html, dcc, dash_table
-from dash_bootstrap_components.themes import BOOTSTRAP
-import plotly
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
+import plotly.graph_objects as go
 
-def load_data(path: str) -> pd.DataFrame:
-    data = pd.read_csv(
-            path
-    )
-    return data
+import pandas as pd
 
-def create_map(app: Dash, data) -> html.Div:
-    return html.Div(
-        className="app-div",
-        children=[
-            html.H1(app.title),
-            dcc.Graph(figure=px.choropleth(data, locations="indicator", locationmode="country names",
-                                           color="population (2018)", hover_name="indicator",
-                                           range_color=[0, 1000], color_continuous_scale="Blues"))
-        ]
-    )
+df = pd.read_csv(
+    'data.csv')
 
-def create_chart(app: Dash, data) -> html.Div:
-    df = px.data.iris()
-    fig = px.scatter(df, x="sepal_width", y="sepal_length")
-    return html.Div(
-        className="app-div1",
-        children=[
-            html.H1(app.title),
-            dcc.Graph(figure=fig)
-        ]
-    )
+app = Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id='c_graph'),
+    dcc.Dropdown(options=df["ISO Country code (2018)"],
+                 value='USA', id='selected-country'),
+])
 
 
+@app.callback(
+    Output('c_graph', 'figure'),
+    Input('selected-country', 'value'))
+def update_figure(selected_country):
 
-def main() -> None:
-    data = load_data("data.csv")
-    app = Dash(external_stylesheets=[BOOTSTRAP])
-    app.title = "471 Final Project"
-    app.layout = create_map(app, data=data)
-    app.layout = create_chart(app, data=data)
-    app.run()
+    df_m = df
 
+    df_m.replace(',', '', regex=True, inplace=True)
 
+    df_m = df_m.astype({'population (2018)': 'float64'})
 
+    mid = df_m.iloc[df_m.index[df_m["ISO Country code (2018)"]
+                               == selected_country]]["population (2018)"].values[0]
 
+    df_m["adjusted"] = df_m.apply(
+        lambda row: ((mid - row["population (2018)"]) / row["population (2018)"]) * 100, axis=1)
 
-if __name__ == "__main__":
-    main()
+    max = df_m["adjusted"].max()
+
+    min = df_m["adjusted"].min()
+
+    print(df_m['adjusted'])
+
+    fig = go.Figure(go.Choropleth(
+        locations=df_m['ISO Country code (2018)'],
+        z=df_m["adjusted"],
+        zmax=600,
+        zmin=-100,
+        colorscale='Turbo',
+        autocolorscale=False,
+        reversescale=True,
+        marker_line_color='darkgray',
+        colorbar_title='Adjusted Population Difference',
+        colorbar=dict(thickness=20, ticklen=3, outlinewidth=0),
+        marker_line_width=0.5,
+    ))
+
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
